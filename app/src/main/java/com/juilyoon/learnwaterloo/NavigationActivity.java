838dfc87a800f3a8589fdb4e9136f8f830375eac
Juil Yoon <jiy.juil@gmail.com>
2016-06-16 14:06:37 -0400

    Rename activities

--- app/src/main/java/com/juilyoon/learnwaterloo/NavigationActivity.java
+++ app/src/main/java/com/juilyoon/learnwaterloo/NavigationActivity.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class Navigation extends AppCompatActivity {
+public class NavigationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +20,7 @@ public class Navigation extends AppCompatActivity {
         setContentView(R.layout.activity_section);
         switch (view.getId()) {
             case R.id.section1:
-                Intent section = new Intent(this, Section.class);
+                Intent section = new Intent(this, SectionActivity.class);
                 startActivity(section);
         }
     }
--- app/src/main/java/com/juilyoon/learnwaterloo/SectionActivity.java
+++ app/src/main/java/com/juilyoon/learnwaterloo/SectionActivity.java
@@ -2,17 +2,12 @@ package com.juilyoon.learnwaterloo;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.text.Html;
-import android.widget.TextView;
 
-public class Section extends Activity {
+public class SectionActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_section);
-
-        TextView body = (TextView) findViewById(R.id.body);
-        body.setText(Html.fromHtml("<h1>Section 1</h1>"));
     }
 }
