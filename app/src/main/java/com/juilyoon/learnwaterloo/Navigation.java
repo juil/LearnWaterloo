f21032b0a98fcfa5892c3e1265bd2fb8b5f16d62
Juil Yoon <jiy.juil@gmail.com>
2016-06-16 11:43:26 -0400

    Initial commit

---
+++ app/src/main/java/com/juilyoon/learnwaterloo/Navigation.java
@@ -0,0 +1,13 @@
+package com.juilyoon.learnwaterloo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Navigation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation);
+    }
+}
