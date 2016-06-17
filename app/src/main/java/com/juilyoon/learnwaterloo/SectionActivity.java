6e8b1c701b08586b6781f8142ddd4bbceaebb7a1
Juil Yoon <jiy.juil@gmail.com>
2016-06-17 16:33:09 -0400

    Make NavigationActivity the parent of SectionActivity

--- app/src/main/java/com/juilyoon/learnwaterloo/SectionActivity.java
+++ app/src/main/java/com/juilyoon/learnwaterloo/SectionActivity.java
@@ -1,7 +1,10 @@
 package com.juilyoon.learnwaterloo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
 
 public class SectionActivity extends Activity {
 
@@ -9,5 +12,15 @@ public class SectionActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_section);
+
+        TextView body = (TextView) findViewById(R.id.body);
+        body.setText("Section 1");
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(this, NavigationActivity.class));
+        this.finish();
     }
 }
