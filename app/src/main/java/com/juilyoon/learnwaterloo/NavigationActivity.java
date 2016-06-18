48453ac84939376a3b2246277b224135986771ae
Juil Yoon <jiy.juil@gmail.com>
2016-06-18 17:20:32 -0400

    Load markdown as HTML on load

--- app/src/main/java/com/juilyoon/learnwaterloo/NavigationActivity.java
+++ app/src/main/java/com/juilyoon/learnwaterloo/NavigationActivity.java
@@ -3,9 +3,11 @@ package com.juilyoon.learnwaterloo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class NavigationActivity extends AppCompatActivity {
+    public final static String EXTRA_FILE = "com.juilyoon.learnwaterloo.file";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +20,28 @@ public class NavigationActivity extends AppCompatActivity {
      */
     public void goToSection(View view) {
         setContentView(R.layout.activity_section);
+        Intent section = new Intent(this, SectionActivity.class);
+        int file = 0;
         switch (view.getId()) {
             case R.id.section1:
-                Intent section = new Intent(this, SectionActivity.class);
-                startActivity(section);
+                file = R.raw.section1;
+                break;
+            case R.id.section2:
+                file = R.raw.section2;
+                break;
+            case R.id.section3:
+                file = R.raw.section3;
+                break;
+            case R.id.section4:
+                file = R.raw.section4;
+                break;
+            case R.id.section5:
+                file = R.raw.section5;
+                break;
         }
+        section.putExtra(EXTRA_FILE, file);
+        Log.v("NavigationActivity", "Opening file " + file);
+        startActivity(section);
     }
 
 }
--- app/src/main/java/com/juilyoon/learnwaterloo/SectionActivity.java
+++ app/src/main/java/com/juilyoon/learnwaterloo/SectionActivity.java
@@ -1,10 +1,25 @@
 package com.juilyoon.learnwaterloo;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.os.Environment;
 import android.text.Html;
+import android.util.Log;
+import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toolbar;
+
+import org.commonmark.html.HtmlRenderer;
+import org.commonmark.node.*;
+import org.commonmark.parser.Parser;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class SectionActivity extends Activity {
 
@@ -13,8 +28,22 @@ public class SectionActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_section);
 
-        TextView body = (TextView) findViewById(R.id.body);
-        body.setText("Section 1");
+
+//        File file = file;
+
+        Parser parser = Parser.builder().build();
+        Node document = parser.parse("# Section Test");
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+
+        Intent intent = getIntent();
+        int fileId = intent.getIntExtra(NavigationActivity.EXTRA_FILE, R.raw.error);
+
+        TextView body = new TextView(this);
+        body.setText(Html.fromHtml(renderer.render(document)));
+
+        ScrollView scrollView = (ScrollView) findViewById(R.id.mainScroll_view);
+        scrollView.addView(body);
+        Log.v("SectionAcitivity", "Trying to open file " + intent.getIntExtra(NavigationActivity.EXTRA_FILE, R.raw.error));
     }
 
     @Override
@@ -23,4 +52,13 @@ public class SectionActivity extends Activity {
         startActivity(new Intent(this, NavigationActivity.class));
         this.finish();
     }
+
+//    private String readRawFile(Context ctx, int resId) {
+//        InputStream inputStream = ctx.getResources().openRawResource(fileId);
+//        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+//        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+//
+//        return "Testing";
+//    }
+
 }
